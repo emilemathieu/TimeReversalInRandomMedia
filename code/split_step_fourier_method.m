d986715fde385cf674d9cd08fdeba5c8f53873c1
Emile Mathieu <emilem@bamlab.fr>
2017-03-18 22:11:37 +0100

    bug fix

--- code/split_step_fourier_method.m
+++ code/split_step_fourier_method.m
@@ -1,14 +1,15 @@
-function [ U ] = split_step_fourier_method(start, step, ending, u0, h, k, n, GP_seq)
+function [ U ] = split_step_fourier_method(start, step, ending, u0, h, k, grid_f, GP_seq)
     u = u0;
+    grid_f = fftshift(grid_f);
+    c = fft(u);
     for m = start:step:ending % Start time loop
+        c = exp(-h/2/k*1i*(grid_f).^2).*c; % Advance in Fourier Space
+        u=ifft(c);
         if nargin > 7
             mu = GP_seq(floor(h*m)+1,:)';
-            u = exp(h/2*1i*k*mu).*u; % Solve non-constant part of LSE
+            u = exp(h/2*1i*k.*mu).*u; % Solve non-constant part of LSE
         end
-        c = fftshift(fft(u)); % Take Fourier transform
-        c = exp(-h/2/k*1i*(2*pi*n).^2).*c; % Advance in Fourier Space
-        u = ifft(fftshift(c)); % Return to Physical Space
+        c=fft(u);
     end
-    U =u;
-end
+    U = ifft(c);
 
